1b34a775eafee563cdebfe96d2e798cf50837161
Melvin0070 <melvin.aspirant@gmail.com>
2024-04-20 16:58:59 +0530

    Schema designed

---
+++ backend/models/userSchema.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const studentSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  interests: [{ type: String }],
+  registeredEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+  bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+});
+
+const organizerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  college: { type: String, required: true },
+  events: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+});
+const User = mongoose.model("users", studentSchema);
+const Organizer = mongoose.model("organizers", organizerSchema);
+
+module.exports = { User, Organizer };
