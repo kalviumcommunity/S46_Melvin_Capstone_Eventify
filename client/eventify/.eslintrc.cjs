46f65d4194521b3e1ba16881160c35b359614711
Melvin0070 <melvin.aspirant@gmail.com>
2024-05-13 10:31:25 +0530

    removed react-refresh plugin from ESLint config

--- client/eventify/.eslintrc.cjs
+++ client/eventify/.eslintrc.cjs
@@ -10,7 +10,6 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
